809599ed03296e4d3605b2861cba7dba65a579a0
yaamchaa <yaamchaa@gmail.com>
2026-06-16 18:58:25 +0900

    Update ScrollToTop.tsx

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 export default function ScrollToTop() {
   const { pathname, search, hash, key } = useLocation();
@@ -8,6 +8,21 @@ export default function ScrollToTop() {
     window.scrollTo(0, 0);
     document.documentElement.scrollTo(0, 0);
     document.body.scrollTo?.(0, 0);
+
+    const scrollableElements = Array.from(
+      document.querySelectorAll<HTMLElement>('*')
+    ).filter((el) => {
+      const style = window.getComputedStyle(el);
+      const overflowY = style.overflowY;
+      const canScroll =
+        (overflowY === 'auto' || overflowY === 'scroll') &&
+        el.scrollHeight > el.clientHeight;
+      return canScroll;
+    });
+
+    scrollableElements.forEach((el) => {
+      el.scrollTo(0, 0);
+    });
   }, [pathname, search, hash, key]);
 
   return null;
